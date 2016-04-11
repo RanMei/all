aadffe86d7354ff167665cbb9ea4ecbc1b6fcb53
赵璐炜 <luwei.zhao@tulingdao.com>
2016-04-11 19:13:50 +0800

    update

--- $tpl/wheelpicker.js
+++ $tpl/wheelpicker.js
@@ -3,73 +3,92 @@
 	$.fn.isWheelPicker = function( params ){
 		
 		var $$wheelPicker	= this;
+
+		// params.items -> $$items
+		var $$names			= params.names,
+			$$items			= params.items,
+			$$itemHeight	= params.itemHeight || 30;
 		var $$wheelNumber	= params.wheelNumber || 3;
 
 		$$wheelPicker.css({
 			overflow:"hidden"
 		});
 
-		var items = [
-			"1",
-			"2",
-			"3",
-			"4",
-			"5",
-			"6",'7','8',9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28
-		];
+		// function renderTemplate () {
+		// 	var inserted = '';
+		// 	for(var i=0;i<items.length;i++){
+		// 		inserted+='<li style="height:30px;line-height:30px;text-align:center">'+items[i]+'</li>';
+		// 	}
+		// 	for( var i=0;i<$$wheelNumber;i++ ){
+		// 		$$wheelPicker.append(
+		// 			'<div class="part" style="float:left;">'+
+		// 				'<div class="itemPicked" style="height:30px;"></div>'+
+		// 				'<div class="window" style="position:relative;width:200px;height:150px;background:lightgrey;overflow:hidden;">'+
+		// 					'<ul class="wheel" style="background:lightgrey;">'+
+		// 						inserted+
+		// 					'</ul>'+
+		// 				'</div>'+
+		// 			'</div>'
+		// 		);
+		// 	};
+		// }
+		// renderTemplate();
 
-		function renderTemplate () {
-			var inserted = '';
-			for(var i=0;i<items.length;i++){
-				inserted+='<li style="height:30px;line-height:30px;text-align:center">'+items[i]+'</li>';
-			}
-			for( var i=0;i<$$wheelNumber;i++ ){
-				$$wheelPicker.append(
-					'<div class="part" style="float:left;">'+
-						'<div class="itemPicked" style="height:30px;"></div>'+
-						'<div class="window" style="position:relative;width:200px;height:150px;background:lightgrey;overflow:hidden;">'+
-							'<ul class="wheel" style="background:lightgrey;">'+
-								inserted+
-							'</ul>'+
-						'</div>'+
-					'</div>'
-				);
-			};
-		}
-		renderTemplate();
-
-		function creatWheel ( items,i ) {
-
-			var $$itemHeight	= params.itemHeight || 30;
+		function creatWheel ( name,items ) {
 
 			var $$itemPicked = 0;
-
+			// @state
 			var isMouseDown = false;
 			var originalY,y1,t1,t2;
 			var interval;
 
 			function render(){
-				var thisWheel = $$wheelPicker.find(".wheel").eq(0);
-				
+				$$wheelPicker.append(
+					'<div class="part '+ name +'" style="float:left;">'+
+						'<div class="itemPicked" style="height:30px;"></div>'+
+						'<div class="window" style="position:relative;width:200px;height:'+(5*$$itemHeight)+'px;background:lightgrey;overflow:hidden;">'+
+							'<ul class="wheel" style="background:lightgrey;">'+
+							'</ul>'+
+						'</div>'+
+					'</div>'
+				);
+				for(var i=0;i<items.length;i++){
+					$$wheelPicker.find("."+name).find(".wheel").append('<li style="height:'+$$itemHeight+'px;line-height:30px;text-align:center">'+items[i]+'</li>');
+				}				
 			};
 			render();
 
-			var thisWindow		= $$wheelPicker.find('.window').eq(i);
-			var thisItemPicked	= $$wheelPicker.find('.itemPicked').eq(i);
-			var thisWheel		= $$wheelPicker.find('.wheel').eq(i);
+			var thisWindow		= $$wheelPicker.find("."+name).find(".window");
+			var thisItemPicked	= $$wheelPicker.find("."+name).find(".itemPicked");
+			var thisWheel		= $$wheelPicker.find("."+name).find(".wheel");
 
 			function wheel(items,itemHeight,itemPicked) {
 				this.items = items;
 			}
 
+			// This function will 
 			function setPosition() {
 				var marginTop = thisWheel.css("marginTop").replace(/px/,'');
-				if( marginTop>60 ){
-					thisWheel.animate({marginTop:60},200,callback);
-				}else if( marginTop<-(items.length-3)*30 ){
-					thisWheel.animate({marginTop:-(items.length-3)*30},200,callback);
+				var marginTopMax = (2*$$itemHeight);
+				var marginTopMin = -(items.length-3)*$$itemHeight;
+				if( marginTop>marginTopMax ){
+					thisWheel.animate(
+						{marginTop:marginTopMax},
+						200,
+						callback
+					);
+				}else if( marginTop<marginTopMin ){
+					thisWheel.animate(
+						{marginTop:marginTopMin},
+						200,
+						callback
+					);
 				}else{
-					thisWheel.animate({marginTop:Math.round( marginTop/30 )*30},100,callback);
+					thisWheel.animate(
+						{marginTop:Math.round( marginTop/$$itemHeight )*$$itemHeight},
+						100,
+						callback
+					);
 				}
 				function callback() {
 					$$itemPicked = ( 2 - (thisWheel.css("marginTop").replace(/px/,'')/$$itemHeight) );
@@ -83,14 +102,14 @@
 				clearInterval(interval);
 				setPosition();
 				isMouseDown = true;
-				originalY = e.pageY||e.originalEvent.changedTouches[0].pageY;
+				originalY = e.pageY || e.originalEvent.changedTouches[0].pageY;
 				y1 = e.pageY||e.originalEvent.changedTouches[0].pageY;
 				t1 = new Date().getTime();
 			});
 
 			thisWindow.on("mousemove touchmove",function(e){
-				if(isMouseDown){
-					var currentY = e.pageY||e.originalEvent.changedTouches[0].pageY;
+				if( isMouseDown ){
+					var currentY = e.pageY || e.originalEvent.changedTouches[0].pageY;
 					var distance = currentY - y1;
 					y1 = currentY;
 					//console.log(distance);
@@ -108,7 +127,7 @@
 					if(currentY-originalY>0){
 						interval = setInterval(function(){
 							thisWheel.css({marginTop:"+=4px"});
-							if( thisWheel.css("marginTop").replace(/px/,'')>=60 ){
+							if( thisWheel.css("marginTop").replace(/px/,'')>=(2*$$itemHeight) ){
 								clearInterval(interval);
 								setPosition();
 							}
@@ -116,7 +135,7 @@
 					}else if(currentY-originalY<0){
 						interval = setInterval(function(){
 							thisWheel.css({marginTop:"-=4px"});
-							if( thisWheel.css("marginTop").replace(/px/,'')<=-(items.length-3)*30 ){
+							if( thisWheel.css("marginTop").replace(/px/,'')<=-(items.length-3)*$$itemHeight ){
 								clearInterval(interval);
 								setPosition();
 							}
@@ -139,9 +158,10 @@
 			});
 		};
 
-		creatWheel(items,0);
-		creatWheel(items,1);
-		creatWheel(items,2);
+		for( var i=0;i<$$names.length;i++ ){
+			creatWheel( $$names[i],$$items[i] );
+		};
+
 	};
 
 })( jQuery );
