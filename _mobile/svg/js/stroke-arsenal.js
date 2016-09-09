89cfc0ce127723c9616e6f63b841887fb80426ab
unknown <zhaoluwei@wenxue.root>
2016-09-09 18:16:07 +0800

    good

---
+++ _mobile/svg/js/stroke-arsenal.js
@@ -0,0 +1,46 @@
+function Arsenal(){
+	this.wheel = document.querySelector('#wheel');
+	this.cannon = document.querySelector('#cannon');
+	this.blue = document.querySelectorAll('.blue');
+	this.template = document.querySelector('#template');
+	this.plate1 = document.querySelector('#plate1');
+	this.plate2 = document.querySelector('#plate2');
+
+	this.cannonFill = document.querySelector('#cannon-fill');
+
+	this.textStroke = document.querySelector('.text-stroke');
+	this.textFill = document.querySelector('.text-fill');
+	this.init();
+}
+Arsenal.prototype = {
+	init: function(){
+		var self = this;
+		self.wheel.style.animation = '2s wheel forwards';
+		setTimeout(function(){
+			self.cannon.style.animation = '2s cannon forwards';
+		},1000);
+		setTimeout(function(){
+			self.blue[0].style.animation = '2s blue forwards';
+			self.blue[1].style.animation = '2s blue forwards';
+		},2500);
+		setTimeout(function(){
+			self.template.style.animation = '2s template forwards';
+		},3000);
+		setTimeout(function(){
+			self.wheel.style.fill = '#A39161';
+			self.cannonFill.style.animation = '1s cannon-fill ease-out forwards';
+			self.cannon.style.strokeWidth = 3;
+			self.blue[0].style.fill = '#003876';
+			self.blue[1].style.fill = '#003876';
+			self.plate1.style.fill = '##EC1C2E';
+			self.plate2.style.fill = '#CE1141';
+			self.template.style.fill = '#A39161';
+			self.template.style.stroke = '#A39161';
+		},5000);
+		setTimeout(function(){
+			self.textStroke.style.animation = '1s text forwards';
+			self.textFill.style.animation = '1s text forwards';
+		},6000);
+	}
+}
+new Arsenal();
