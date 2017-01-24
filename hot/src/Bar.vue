5274318c98d29590683a6c288da0b8d8ce6d1ebf
soap <403177368@qq.com>
2017-01-24 15:01:33 +0800

    hmr

---
+++ hot/src/Bar.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="Bar">
+		{{text}}
+		{{text}}
+	</div>
+</template>
+
+<style lang="less" scoped>
+	.Bar {
+		width: 80%;
+		padding: 10px;
+		margin: auto;
+		background: orange;
+
+	}
+</style>
+
+<script type="text/javascript">
+	export default {
+		data: function(){
+			return {
+				text: 'dogddddd'
+			}
+		}
+	}
+</script>
